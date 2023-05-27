615788ec4d7ff85ab334e37b3dfa081421edf0ca
Alek Pialucha <pialuchaa@btinternet.com>
2023-05-27 03:02:11 +0100

    started on homepage and editted bootstrap colours

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'artworks/index'
-  get 'artworks/show'
-  get 'artworks/new'
-  get 'artworks/create'
-  get 'artworks/destroy'
-  get 'artworks/edit'
-  get 'artworks/update'
-  get 'users/artworks'
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :artworks
+  root to: "artworks#home"
 end
