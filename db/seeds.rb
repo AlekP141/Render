17922a3d721fd4e3a6739bfe4cf8b9c70ab21149
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-05-29 17:50:42 +0100

    Created seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Artwork.destroy_all
-User.destroy_all
 require "faker"
 
+Artwork.destroy_all
+User.destroy_all
+
 5.times do
-  User.create!(
+  user = User.new(
     first_name: Faker::GreekPhilosophers.name,
     last_name: Faker::GreekPhilosophers.name,
     email: Faker::Internet.email,
     username: Faker::Internet.username(specifier: 5..10),
-    # password: Faker::Internet.password(min_length: 8)
+    password: Faker::Internet.password(min_length: 8)
   )
-end
+  user.save
 
-20.times do
-  Artwork.create!(
-    name: Faker::GreekPhilosophers.name,
-    price: rand(10..40),
-    width: rand(50..150),
-    height: rand(50..150),
-    average_rating: rand(0.0..10.0),
-    address: Faker::Address.street_address,
-    description: Faker::Quote.matz
-  )
+  4.times do
+    artwork = Artwork.new(
+      name: Faker::GreekPhilosophers.name,
+      price: rand(10..40),
+      width: rand(50..150),
+      height: rand(50..150),
+      average_rating: rand(0.0..10.0).round(2),
+      address: Faker::Address.street_address,
+      description: Faker::Quote.matz,
+      user: user
+    )
+    artwork.save
+  end
 end
