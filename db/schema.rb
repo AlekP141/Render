114d0d4569a400e66e44817a58a7fd6145f33289
Dylon M <dylonmura@gmail.com>
2023-05-29 17:13:50 +0100

    dylon/ db clearing + model creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_145138) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_161311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_145138) do
     t.index ["user_id"], name: "index_artworks_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "artwork_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artwork_id"], name: "index_bookings_on_artwork_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.float "rating"
     t.text "comment"
@@ -39,16 +50,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_145138) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
-    t.string "email"
     t.string "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "artworks", "users"
+  add_foreign_key "bookings", "artworks"
+  add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "artworks"
   add_foreign_key "reviews", "users"
 end
