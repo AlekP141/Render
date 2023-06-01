8b60d2f515c5f07fe7f42adf68d3747234f13b66
Dylon M <dylonmura@gmail.com>
2023-06-01 16:17:51 +0100

    mapbox add

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
         style: "mapbox://styles/mapbox/streets-v10"
       });
       this.#addMarkersToMap()
-      console.log(this.markersValue);
+      this.#fitMapToMarkers()
     }
 
     #addMarkersToMap(){
@@ -24,4 +24,11 @@ export default class extends Controller {
           .addTo(this.map)
     })
   }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  }
+
 }
