31369770717e4b85dbae0556f7b3737260804dee
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-06-01 10:32:33 +0100

    Doing the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,20 @@
 class ReviewsController < ApplicationController
-  def new
-  end
-
   def create
+    @review = Review.new(review_params)
+    @artwork = Artwork.find(params[:artwork_id])
+    @review.artwork = @artwork
+    redirect_to artwork_path(@artwork) if @review.save
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to list_path(@review.list)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
