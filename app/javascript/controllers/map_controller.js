1d5ecece7657bdc9d36d853186648c0455dc8513
Dylon M <dylonmura@gmail.com>
2023-06-01 17:07:02 +0100

    dylon-completed-mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
         style: "mapbox://styles/mapbox/streets-v10"
       });
       this.#addMarkersToMap()
-      console.log(this.markersValue);
+      this.#fitMapToMarkers()
     }
 
     #addMarkersToMap(){
@@ -23,5 +23,10 @@ export default class extends Controller {
           .setLngLat([marker.lng, marker.lat])
           .addTo(this.map)
     })
+  }
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,9 @@ Booking.destroy_all
 Artwork.destroy_all
 User.destroy_all
 
-def createArtworks(user)
+counter = 0
+
+def createArtworks(user, counter)
   image_urls = [
     "https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80",
     "https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1045&q=80",
@@ -27,32 +29,21 @@ def createArtworks(user)
     "https://images.unsplash.com/photo-1598565061944-fdd9f2775f98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
     "https://images.unsplash.com/photo-1549289524-06cf8837ace5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fGNsYXNzaWMlMjBhcnR8ZW58MHx8MHx8fDI%3D&auto=format&fit=crop&w=900&q=60",
     "https://images.unsplash.com/photo-1578926314433-e2789279f4aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1010&q=80",
-    "https://images.unsplash.com/photo-1578321271369-d008a1ee4fd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1031&q=80",
-    "https://images.unsplash.com/photo-1578301977947-cb446e84e8cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1023&q=80"
+    "https://images.unsplash.com/photo-1584446922442-7ac6b8c118f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1056&q=80",
+    "https://images.unsplash.com/photo-1577398628395-4ebd1f36731b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80"
   ]
 
   address = [
     "138 Kingsland Rd London E2 8DY",
-    "10 Downing Street Westminster London SW1A 2AA",
-    "221B Baker Street Marylebone London NW1 6XE",
-    "160 Great Portland Street Fitzrovia London W1W 5QA",
+    "1 Canada Square Canary Wharf London E14 5AB",
+    "40 Liverpool Street City of London London EC2M 7QN",
     "1 Abbey Road St John's Wood London NW8 9AY",
     "32 London Bridge Street Southwark London SE1 9SG",
     "221 Strand Covent Garden London WC2R 1AP",
     "123 Oxford Street Soho London W1D 2HU",
-    "28 Portobello Road Notting Hill London W11 3DG",
-    "66 The Cut Waterloo London SE1 8LZ",
-    "1 St Martin's Le Grand City of London London EC1A 4NP",
-    "30 Leicester Square West End London WC2H 7LA",
-    "160 Oxford Street Westminster London W1D 1NN",
-    "45 St. James's Place St. James's London SW1A 1NH",
-    "9 Savile Row Mayfair London W1S 3PF",
-    "20 Fenchurch Street City of London London EC3M 3BY",
-    "10 Hanover Square Mayfair London W1S 1JH",
-    "1 Canada Square Canary Wharf London E14 5AB",
-    "40 Liverpool Street City of London London EC2M 7QN",
-    "77 Baker Street Marylebone London W1U 6RF",
-    "18 Victoria Embankment Westminster London EC4Y 0DH"
+    "42 Harrow View Road Ealing London W5 1LZ",
+    "22 Hampstead High Street Hampstead London NW3 1QD",
+    "98 Tottenham Lane Hornsey London N8 7EE",
   ]
 
   4.times do
@@ -62,15 +53,17 @@ def createArtworks(user)
       width: rand(50..150),
       height: rand(50..150),
       average_rating: rand(3.0..5.0).round(2),
-      address: address.sample,
+      address: address[counter],
       description: Faker::Quote.matz,
       user: user
     )
-    file = URI.open(image_urls.sample)
+    file = URI.open(image_urls[counter])
     artwork.photo.attach(io: file, filename: "#{artwork.name}.png", content_type: "image/png")
     artwork.save
     puts "Artwork #{artwork.id} saved"
+    counter += 1
   end
+  return counter
 end
 
 user1 = User.create(
@@ -81,7 +74,7 @@ user1 = User.create(
   password: "123456"
 )
 puts "User #{user1.id} created"
-createArtworks(user1)
+counter = createArtworks(user1, counter)
 
 user2 = User.create(
   first_name: "Alek",
@@ -91,7 +84,7 @@ user2 = User.create(
   password: "123456"
 )
 puts "User #{user2.id} created"
-createArtworks(user2)
+counter = createArtworks(user2, counter)
 
 user3 = User.create(
   first_name: "Lorenzo",
@@ -101,4 +94,6 @@ user3 = User.create(
   password: "123456"
 )
 puts "User #{user3.id} created"
-createArtworks(user3)
+counter = createArtworks(user3, counter)
+
+puts counter
