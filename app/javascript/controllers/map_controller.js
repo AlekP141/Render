23974fa6be27918c928342b8b6419cd6475bd6ae
Dylon M <dylonmura@gmail.com>
2023-06-02 12:23:32 +0100

    dylon font change

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
       mapboxgl.accessToken = this.apiKeyValue
       this.map = new mapboxgl.Map({
         container: this.element,
-        style: "mapbox://styles/mapbox/streets-v10"
+        style: "mapbox://styles/mapbox/streets-v12"
       });
       this.#addMarkersToMap()
       this.#fitMapToMarkers()
